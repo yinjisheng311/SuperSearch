a70eb89ca26406ff7a68fc399da8af17aeeb13be
yinjisheng <jisheng_yin@mymail.sutd.edu.sg>
2018-07-13 15:34:14 +0800

    button at top right

--- googleSearchContentScript.js
+++ googleSearchContentScript.js
@@ -44,37 +44,50 @@ function get_link_from_html(innerhtml) {
 }
 
 
-function get_search_query(){
-	return document.getElementById('lst-ib').value
+function get_search_query() {
+    return document.getElementById('lst-ib').value
 }
 
 
-function insert_analysis_iframe(){
-	// TODO This function will wait for data response from api script 
-	var top_result_bar = document.getElementById("appbar");
-	
-	console.log('this is loading');
-	var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
-	if (!location.ancestorOrigins.contains(extensionOrigin)) {
-	    var iframe = document.createElement("iframe");
-	    iframe.src = chrome.runtime.getURL("/components/iframe-graph/iframe-graph.html");
-	    iframe.width = '1000';
-	    iframe.height = '200';
-		iframe.className = '.super_iframe';
-	    top_result_bar.insertAdjacentElement("afterend", iframe);
-	}
-	console.log('this is loaded');
+function insert_analysis_iframe() {
+    // TODO This function will wait for data response from api script 
+    var top_result_bar = document.getElementById("appbar");
+
+    console.log('inserting iframe');
+    var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+    if (!location.ancestorOrigins.contains(extensionOrigin)) {
+        var iframe = document.createElement("iframe");
+        iframe.src = chrome.runtime.getURL("/components/iframe-graph/iframe-graph.html");
+        iframe.width = '1000';
+        iframe.height = '200';
+        iframe.className = '.super_iframe';
+        top_result_bar.insertAdjacentElement("afterend", iframe);
+    }
+    console.log('inserting graph button');
+    var result_stats_bar = document.getElementById("resultStats");
+    var download_button = document.createElement("button");
+
+
+    download_button.setAttribute("class", "mdl-button mdl-js-button mdl-button--raised");
+    // download_button.innerHTML = "Relationship Graph";
+    download_button.setAttribute("style", "float:right; margin-right:1rem; margin-top:1rem;");
+    var versions_icon = document.createElement("i");
+    versions_icon.setAttribute("class", "material-icons")
+    versions_icon.innerHTML = "mood";
+    download_button.appendChild(versions_icon);
+    result_stats_bar.insertAdjacentElement("afterend", download_button);
+
 }
 
 
 function main() {
     if (is_page_at_google_all_menu()) {
         var links = scrape_search_results();
-		var query = get_search_query();
+        var query = get_search_query();
 
         console.log("List of links ====");
         console.log(links);
-		console.log("Query:"+query)
+        console.log("Query:" + query)
         // send to background.js
 
         var message = {
@@ -82,9 +95,9 @@ function main() {
             query: query,
             links: links
         };
-		console.log(message);
+        console.log(message);
         chrome.runtime.sendMessage(message);
-		insert_analysis_iframe()
+        insert_analysis_iframe()
     }
 }
 
--- insert-iframe-graph.js
+++ insert-iframe-graph.js
@@ -1,80 +1,27 @@
 console.log('super search launched');
 
+
+var result_stats_bar = document.getElementById("resultStats");
+var download_button = document.createElement("button");
+
+
+download_button.setAttribute("class", "mdl-button mdl-js-button mdl-button--raised");
+// download_button.innerHTML = "Relationship Graph";
+download_button.setAttribute("style", "float:right; margin-right:1rem; margin-top:1rem;");
+var versions_icon = document.createElement("i");
+versions_icon.setAttribute("class", "material-icons")
+versions_icon.innerHTML = "mood";
+download_button.appendChild(versions_icon);
+result_stats_bar.insertAdjacentElement("afterend", download_button);
+
 var top_result_bar = document.getElementById("appbar");
-// var graph_div = document.createElement('div');
-// graph_div.setAttribute("id", "cy");
-// main_body_div.appendChild(graph_div);
-
-// var download_button = document.createElement("button");
-// download_button.setAttribute("class", "mdl-button mdl-js-button mdl-button--raised");
-// download_button.innerHTML = "whatever";
-// main_body_div.appendChild(download_button);
-
-
 console.log('this is loading');
 var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
 if (!location.ancestorOrigins.contains(extensionOrigin)) {
-    // var script3 = document.createElement("script");
-    // script3.defer = true;
-    // script3.src = chrome.runtime.getURL("cytoscape.js");
-    // node_container_div.appendChild(script3);
     var iframe = document.createElement("iframe");
     iframe.src = chrome.runtime.getURL("/components/iframe-graph/iframe-graph.html");
     iframe.width = '1000';
     iframe.height = '200';
     top_result_bar.insertAdjacentElement("afterend", iframe);
-
-    // top_result_bar.appendChild(iframe);
 }
 console.log('this is loaded');
-
-{/* <script src="cytoscape.js"></script>
-<script>
-    var cy = cytoscape({
-        container: document.getElementById('cy'),
-        elements: [ // list of graph elements to start with
-            { // node a
-                data: {
-                    id: 'a'
-                }
-            },
-            { // node b
-                data: {
-                    id: 'b'
-                }
-            },
-            { // edge ab
-                data: {
-                    id: 'ab',
-                    source: 'a',
-                    target: 'b'
-                }
-            }
-        ],
-
-        style: [ // the stylesheet for the graph
-            {
-                selector: 'node',
-                style: {
-                    'background-color': '#666',
-                    'label': 'data(id)'
-                }
-            },
-
-            {
-                selector: 'edge',
-                style: {
-                    'width': 3,
-                    'line-color': '#ccc',
-                    'target-arrow-color': '#ccc',
-                    'target-arrow-shape': 'triangle'
-                }
-            }
-        ],
-
-        layout: {
-            name: 'grid',
-            rows: 1
-        }
-    });
-</script> */}
